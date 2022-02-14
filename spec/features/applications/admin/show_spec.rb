28b15261ae62eacdf2849f9ee45d5595e49ae903
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-13 18:33:24 -0600

    User story 13 complete

--- spec/features/applications/admin/show_spec.rb
+++ spec/features/applications/admin/show_spec.rb
@@ -22,10 +22,6 @@ RSpec.describe 'the ADMIN application show' do
     end
     
     it 'reject a pet for adoption' do 
-        # IDEAS
-            # SET UP AN ATTRIBUTE IN THE APPLICATIONPETS TABLE FOR STATUS
-            # CHANGE THAT ATTR TO REJECTED OR APPROVED AND UPDATE VIEW ACCORDINGLY
-            
         application = Application.create(name: "Steve Rogers", street_address: "990 Logan St", city: "Denver", state: "CO", zip_code: "80203", status: "Pending")    
         shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
         pet_1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
@@ -45,8 +41,28 @@ RSpec.describe 'the ADMIN application show' do
         expect(page).to_not have_button("Approve Scooby")
         expect(page).to_not have_button("Reject Scooby")
         expect(page).to have_content("Scooby Rejected")
-   
+        
         expect(page).to have_button("Approve Bruce")
         expect(page).to have_button("Reject Bruce")
+    end
+    
+    it 'one application does not affect other applications' do 
+        application_1 = Application.create(name: "Steve Rogers", street_address: "990 Logan St", city: "Denver", state: "CO", zip_code: "80203", status: "Pending")    
+        application_2 = Application.create(name: "Bruce Banner", street_address: "16 Bay Oaks Ct", city: "Lake St. Louis", state: "MO", zip_code: "63367", status: "Pending")
+        shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+        pet_1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+        pet_2 = Pet.create(name: 'Bruce', adoptable: true, age: 4, breed: 'Husky', shelter_id: shelter.id)
+        app_pet_1 = ApplicationPet.create(pet: pet_2, application: application_1)
+        app_pet_2 = ApplicationPet.create(pet: pet_1, application: application_1)
+        app_pet_3 = ApplicationPet.create(pet: pet_1, application: application_2)
+        
+        visit "/admin/applications/#{application_1.id}"
+        click_button("Approve Scooby")
+        expect(page).to have_content("Scooby Approved")
+        
+        visit "/admin/applications/#{application_2.id}"
+        expect(page).to_not have_content("Scooby Approved")
+        expect(page).to have_button("Approve Scooby")
+        expect(page).to have_button("Reject Scooby")
     end
 end
