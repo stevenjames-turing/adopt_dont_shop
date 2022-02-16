0568f5afb25086e303fdfe37e98f125aacc4f0f9
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-16 15:19:48 -0600

    Update testing to use  blocks.

--- spec/features/shelters/admin/show_spec.rb
+++ spec/features/shelters/admin/show_spec.rb
@@ -7,13 +7,28 @@ RSpec.describe 'the admin shelters show' do
         shelter_4 = Shelter.create!(name: 'Arch City Adoptions', city: 'St. Louis, MO', foster_program: true, rank: 4)
 
         visit "/admin/shelters/#{shelter_4.id}"
-
+        
         expect(page).to have_content(shelter_4.name)
         expect(page).to have_content(shelter_4.city)
         expect(page).to_not have_content(shelter_4.rank)
         expect(page).to_not have_content("Pets at Arch City")
         expect(page).to_not have_content(shelter_3.name)
         expect(page).to_not have_content(shelter_3.city)
+    end
+    
+    xit 'has average age of all adoptable pets' do 
+        shelter_4 = Shelter.create!(name: 'Arch City Adoptions', city: 'St. Louis, MO', foster_program: true, rank: 4)
+        pet_10 = Pet.create!(adoptable: true, age: 8, breed: 'Chihuahua', name: 'Nacho', shelter_id: shelter_4.id)
+        pet_11 = Pet.create!(adoptable: true, age: 9, breed: 'Dachsund', name: 'Busch', shelter_id: shelter_4.id)
+        pet_12 = Pet.create!(adoptable: true, age: 2, breed: 'Corgi', name: 'Yoda', shelter_id: shelter_4.id)
+        pet_13 = Pet.create!(adoptable: true, age: 1, breed: 'Border Collie', name: 'Louie', shelter_id: shelter_4.id)
+        pet_14 = Pet.create!(adoptable: true, age: 4, breed: 'Terrier', name: 'Clark', shelter_id: shelter_4.id)
+        pet_15 = Pet.create!(adoptable: true, age: 4, breed: 'Great Dane', name: 'Buff', shelter_id: shelter_4.id)
+        
+        visit "/admin/shelters/#{shelter_4.id}"
+
+        expect(page).to have_content("Shelter Statistics")
+        expect(page).to have_content("Average Age: #{shelter_4.pet_average_age}")
     end
 
 end 
